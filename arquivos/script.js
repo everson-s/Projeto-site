c4d1edb45cecc9a7c3c80d0e47b8df156f124334
Everson dos Santos Pinheiro <79765901+everson-s@users.noreply.github.com>
2022-05-16 20:49:06 -0300

    Update script.js

---
+++ arquivos/script.js
@@ -0,0 +1,34 @@
+let time = 2000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll(".slide img")
+    maxImage = images.length;
+
+function NextImage(){
+    //console.log(images) mostra a lista de imagens
+
+    //console.log(images[1])
+    //Remove a classe selected da imagem corrente
+    images[currentImageIndex].classList.remove("selected") 
+    
+    //incrementa o array de imagens
+    currentImageIndex++
+
+    //condição para voltar à primeira imagem, caso o currentIndex seja maior ou igual ao tamanho do array
+    if(currentImageIndex >= maxImage)
+    currentImageIndex = 0
+
+    //adiciona a classe selected ao array da imagem corrent
+    images[currentImageIndex].classList.add("selected")
+}
+
+function start(){
+    //console.log("Window loaded")
+    setInterval(() => {
+        //console.log("Está funcionando")
+
+        //troca de imagem
+        NextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
